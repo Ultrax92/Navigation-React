f6bd76ce2720d51e87842202280704c209252a1b
Ultrax92 <kiwiihs@gmail.com>
2025-05-13 12:21:53 +0200

    Activité 6

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import { Link } from 'react-router';
-// import "../assets/styles/Header.css"
+import { NavLink } from 'react-router';
+import "../assets/styles/Header.css"
 
 function Header() {
     return (
@@ -8,13 +8,13 @@ function Header() {
             <Navbar bg="light" expand="lg">
                 <Container>
                     <Nav className="ms-auto d-none d-lg-flex">
-                        <Nav.Link as={Link}  to="/" end>
+                        <Nav.Link as={NavLink} to="/" end>
                             Accueil
                         </Nav.Link>
-                        <Nav.Link as={Link} to="/category/vetements">
+                        <Nav.Link as={NavLink} to="/category/vetements">
                             Vêtements
                         </Nav.Link>
-                        <Nav.Link as={Link} to="/category/chaussures">
+                        <Nav.Link as={NavLink} to="/category/chaussures">
                             Chaussures
                         </Nav.Link>
                     </Nav>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,18 @@
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import { Outlet } from "react-router";
+import { Container } from 'react-bootstrap';
 
 const Layout = () => {
     return (
         <>
             <Header />
-            <main className="py-4">
-                <Outlet />
-            </main>
+                <Container className="py-4" as={"main"} >
+                    <Outlet />
+                </Container>
             <Footer />
         </>
     );
 };
+
 export default Layout;
